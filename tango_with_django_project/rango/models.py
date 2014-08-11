377d9265054c4f8d37d2f3d0c30eebe23915ef92
seumoo <sutenee.nopa@gmail.com>
2014-08-10 22:14:37 -0400

    Updated with media, static imgs, templates, models.

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.title
+
+# CharField, a field for storing character data (e.g. strings). Specify max_length to provide a maximum number of characters the field can store.
+# URLField, much like a CharField, but designed for storing resource URLs. You may also specify a max_length parameter.
+# IntegerField, which stores integers.
+# DateField, which stores a Python datetime.date.
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,10 +1,22 @@
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
-def index(request): # one view called index ;
-    # Each view takes in at least one argument - a HttpRequest object, which also lives in the django.http module.
-    # Convention dictates that this is named request, but you can rename this to whatever you want if you so desire
-    return HttpResponse("Rango says: Hello world! <a href='/rango/about'>About</a>") # Each view must return a HttpResponse object. A simple HttpResponse object takes a string parameter
-    # representing the content of the page we wish to send to the client requesting the view
+def index(request):
+    # Request the context of the request.
+    # The context contains information such as the client's machine details, for example.
+    context = RequestContext(request)
+
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
-    return HttpResponse("Rango Says: Here is the about page. <a href= '/rango/'>Index</a>")
+    context = RequestContext(request)
+    context_dict = {'boldmessage2': "Let's return to the index page!"}
+    return render_to_response('rango/about.html', context_dict, context)
